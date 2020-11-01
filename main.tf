848d91aa4e7c1a5cb761ee33c7d31f992152e01d
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2020-11-01 23:29:00 +1100

    Changed resource group name

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "azurerm" {
 }
 
 variable "resource_group_name" {
-  default = "tailspin-space-game-rg"
+  default = "tailspin-space-game-rg-tf"
   description = "The name of the resource group"
 }
 
